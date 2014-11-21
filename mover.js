7d857158455959a4c0efd007195dc791d6d20353
cakesofwrath <dai.alan.dai@gmail.com>
2014-11-21 10:51:22 -0500

    Still no transistions but clean path

--- mover.js
+++ mover.js
@@ -1,82 +1,78 @@
 //plotter
 var TheRoute;
-
-function getTrip(member,StartID,froute){
-	var tripObj = {previousLine:'',currentLine:'',nextLine:'', name:member};
+var updateTripObj;
+var tripStart;
+var getJunctions;
+function getTrip(member,StartID,froute,intervals){
 	var startStation = d3.select("#plot");
 	startStation.append("circle")
 	.attr("class","trip")
 	.attr("id",member)
 	.attr("r","4")
 	.attr("transform",d3.select("#station_"+StartID).attr("transform"));
-	updateTripObj(tripObj,StartID,froute);
-	console.log(tripObj);
-	return tripObj; 
 }
 
-function updateTripObj(tripObj,curStartID,froute){
-	routeMapping(tripObj,curStartID,froute);
-}
-
-function routeMapping(tripObj,stopId,froute){
-	var prev,curr,next,nextEnd;
-	var lines = froute.features;
-	var len = lines.length;
-	for(var i =0; i<len; i++){
-		prev = curr;//set previous stop to what the old current stop was
-		curr = lines[i].start.properties.stop_ids[0].substring(0,3); //set current to our current starting point
-		next = lines[i].end.properties.stop_ids[0].substring(0,3);   //set next to the end of our current lineString
-		if(i<len-1)
-			nextEnd = lines[i+1].end.properties.stop_ids[0].substring(0,3);
-		if(curr === stopId.substring(0,3))
-			break;
+function findJunctions(RouteData){
+	var Junctions = {
+		type:'FeatureCollection',
+		features:[]
+	};
+	RouteData.features.forEach(function(lineString){
+		if(lineString.start.properties.stop_ids[0].indexOf("_")>0)
+			Junctions.features.push(lineString);
+	})
+	console.log("Junctions",Junctions);
+	return function(){
+		return Junctions;
 	}
-	if(prev)
-		tripObj.previousLine = "#route_A_s_"+prev+"_e_"+curr;
-	tripObj.currentLine = "#route_A_s_"+curr+"_e_"+next;
-	tripObj.nextLine = "#route_A_s_"+next+"_e_"+nextEnd;
+}
+function parseTime(s) {
+   	  var formatTime = d3.time.format("%X");
+	  var t = formatTime.parse(s);
+	  if (t != null && t.getHours() < 5) t.setDate(t.getDate() + 1);
+	  return t;
+}
 
+function moveTrip(value,map,trip,intervals){
 	
-}
-
-function moveTrip(value,map,trip){
-	var hours,minutes;
-	if (value instanceof Date){
-		hours = value.getHours();
-		minutes = value.getMinutes();
+	var interval;
+	for(var i =0; i< intervals.length; i++){
+		if(parseTime(intervals[i].start) <= value && value <= parseTime(intervals[i].stop) )
+			interval = intervals[i];
 	}
-
-	var path1 = d3.select(trip.currentLine).node();
-	var l = path1.getTotalLength();
+	
+	var path1 = d3.select("#"+interval.lineID).node();
+	if(path1 == null)
+	{
+		console.log("problems");
+	}
+	var l = path1.getTotalLength();   //get length of the curve
 	var trips = d3.selectAll(".trip");
-	nextStop = trip.currentLine.substr(trip.currentLine.indexOf("_e_") + 3);
-	currStop = trip.currentLine.substr(trip.currentLine.indexOf("_s_")+3,3);
+
 	trips.attr("transform",function(){
-		var t = map(value)/getMaxOfArray(map.range());
-		var p = path1.getPointAtLength(t*l);
-		var next = findStop(nextStop);
-		var curr = findStop(currStop);
-		var d = distance([p.x,p.y], projection(Stops[next].geometry.coordinates) );
-		console.log(d);
-		if(d < 0.0001)
-			updateTripObj(trip,next,route);
+		var shift = map(parseTime(interval.start));
+		var t = (map(value)-shift)/(map(parseTime(interval.stop)) - shift);   //calc % point in time interval
+		
+ 		var p = path1.getPointAtLength(t*l);             //find that % point along the curve
 		return "translate(" + p.x+"," + p.y+")";
 	});
 }
 
 function setTrips(tripData,Element,froute){
 	// console.log(tripData);
-	console.log("The Route",froute);
-
+	TheRoute = froute;
+	getJunctions = findJunctions(TheRoute);
 	var TestTrip = tripData["B20140608WKD_001150_A..S74R"];
 	var trip;
+	console.log(TestTrip);
 	start = TestTrip[0].arrival_time;
 	end = TestTrip[TestTrip.length-1].arrival_time;
-	trip = getTrip("B20140608WKD_001150_A..S74R",TestTrip[0].stop_id,froute);
-	buildSlider(Element,start,end,trip);
+	var intervals = getIntervals(TestTrip,TheRoute);
+	console.log(intervals);
+	trip = getTrip("B20140608WKD_001150_A..S74R",TestTrip[0].stop_id,froute,intervals);
+	buildSlider(Element,start,end,trip,intervals);
 
 }
-
 
 function findStop(stopPrefix){
 	for(var i =0; i< Stops.length; i++){
@@ -86,14 +82,75 @@ function findStop(stopPrefix){
 	return -1;
 }
 
-function timeChunker(tripData,timespan){
-	var timeChunks = [];
-	for(var i = 0; i< tripData.length; i++){
-		var timechunk = [];
-		for(var j=0; j < timespan.length; j++){
-			if( timespan[j] >=tripData[i] && timespan[j]< tripData[i+1])
-				timechunk.push(time)
+function getIntervals(oneTripsData,RouteData){
+	var intervals = [];
+	for(var i =0; i< oneTripsData.length-1; i++){
+		var timeObj = {start_id:'',stop_id:'',start:'', stop:'',lineID:''};
+		timeObj.start_id = oneTripsData[i].stop_id.substring(0,3);
+		timeObj.start = oneTripsData[i].departure_time;
+		timeObj.stop = oneTripsData[i+1].arrival_time;
+		timeObj.stop_id = oneTripsData[i+1].stop_id.substring(0,3);
+		timeObj.lineID = "route_A_s_" + timeObj.start_id +"_e_"+timeObj.stop_id;
+
+		var stop1 = Stops[findStop(timeObj.start_id)].geometry.coordinates;
+		var stop2 = Stops[findStop(timeObj.stop_id)].geometry.coordinates;
+
+		if(inSameSegment(stop1,stop2)){
+			intervals.push(timeObj);
 		}
-		timeChunks.push(timechunk);
+		else{
+			var junction =  findEndJunction(timeObj.stop_id);
+			//AUGMENT GRAPH//
+			graph.bridgeVerticies(timeObj.start_id,junction.start.properties.stop_id,timeObj.stop_id);
+
+			//END AUGMENT GRAPH//
+
+			
+			
+			var timeObj1={};
+			timeObj1.start_id = timeObj.start_id;
+			timeObj1.start = timeObj.start;
+			timeObj1.stop_id = junction.station_name;
+			//divide the time into partitions of the lineString
+			var path = d3.select("#"+timeObj.lineID).node();
+			var date1 = parseTime(timeObj.start);
+			var date2 = parseTime(timeObj.stop);
+			var fraction = Math.ceil( (date1.getTime() + date2.getTime())/2);
+			var form = d3.time.format("%X");
+			var newTime = form(new Date(fraction));
+			timeObj1.stop = newTime;
+			timeObj1.lineID = "route_A_s_"+timeObj1.start_id+"_e_"+timeObj1.stop_id;
+			var timeObj2 ={};
+			timeObj.start_id = timeObj1.stop_id;
+			timeObj2.start = newTime;
+			timeObj2.stop_id = timeObj.stop_id;
+			timeObj2.stop = timeObj.stop;
+			timeObj2.lineID = "route_A_s_"+timeObj2.start_id+"_e_"+timeObj2.stop_id;
+			
+			intervals.push(timeObj1);
+			intervals.push(timeObj2);
+			//console.log(junction);
+			
+		}
 	}
+	console.log(intervals);
+	return intervals;
 }
+
+function findEndJunction(endStop){
+	//hack for bad assumption
+	if(endStop === "H02")
+		endStop = "H01"
+	var junctions = getJunctions().features;
+	var index=0;
+	var len = junctions.length;
+	for(var i =0; i< len; i++){
+		if( junctions[i].start.properties.stop_id.indexOf(endStop) >=0 ){ 
+			return junctions[i];
+		}
+	}
+	return null;
+}
+
+//Best option might be to have a graph datastructure and then a method that 
+//will convert the datastructure to a FeatureColllection after initial processing.
